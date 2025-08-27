62bd22833070403b02197352b48368dcf85f6bbb
wallacevilela7 <wallace.vilela9@gmail.com>
2025-08-27 15:08:40 -0300

    Create spring app with maven and add dependencies to implement e2e tests

---
+++ src/main/java/tech/buildrun/rummye2e/Rummye2eApplication.java
@@ -0,0 +1,13 @@
+package tech.buildrun.rummye2e;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rummye2eApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rummye2eApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/buildrun/rummye2e/Rummye2eApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.buildrun.rummye2e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rummye2eApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
